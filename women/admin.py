a7f59ad110aefccef871444e5dbb6ac6b53b486a
admin <admin@asd-MacBook-Pro.local>
2023-01-11 10:15:20 +0600

    commit

--- women/admin.py
+++ women/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'cat', 'get_html_photo', 'content', 'photo', 'is_published')
+    readonly_fields = ('get_html_photo',)
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{ object.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'Фото'
 
 
 class CategoryAdmin(admin.ModelAdmin):
